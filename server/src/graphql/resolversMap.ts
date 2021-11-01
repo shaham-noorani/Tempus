90e5e5f516ed618d40da8057712e28d6bf68075e
Shaham Noorani <shahamnoorani@gmail.com>
2021-11-01 09:57:55 -0500

    setting up graphql and apollo express

---
+++ server/src/graphql/resolversMap.ts
@@ -0,0 +1,6 @@
+import { IResolvers } from "@graphql-tools/utils";
+import { merge } from "lodash";
+import { UserResolvers } from "./resolvers/UserResolver";
+
+const resolverMap: IResolvers = merge(UserResolvers);
+export default resolverMap;
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,16 @@
 import express from "express";
-
+import { ApolloServer } from "apollo-server-express";
+import { createServer } from "http";
+import schema from "./graphql/schemasMap";
 const app = express();
-const PORT = 8000;
-
-app.get("/", (req, res) => res.send("Hello!"));
-
-app.listen(PORT, () => {
-  console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
+const server = new ApolloServer({
+  schema,
 });
+
+server.applyMiddleware({ app, path: "/graphql" });
+const httpServer = createServer(app);
+httpServer.listen({ port: 3000 }, (): void =>
+  console.log(
+    `\n🚀      GraphQL is now running on http://localhost:3000/graphql`
+  )
+);
