4fcb12d4e31748bbbd63bf67b2d26c10e7a81f11
Shaham Noorani <shahamnoorani@gmail.com>
2021-10-31 23:11:41 -0500

    setting up graphql

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,10 @@
 import express from "express";
+
 const app = express();
 const PORT = 8000;
-app.get("/", (req, res) => res.send("Express + TypeScript Server :)"));
+
+app.get("/", (req, res) => res.send("Hello!"));
+
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
 });
